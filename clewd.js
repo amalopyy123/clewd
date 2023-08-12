500bb5b6e1f8fecd43a076e8dd0e395b92b2b507
amalopyy123 <83387114+amalopyy123@users.noreply.github.com>
2023-08-12 15:57:25 +0900

    debug: set Config.Cookie as string

--- clewd.js
+++ clewd.js
@@ -61,7 +61,7 @@ let uuidOrg;
  * these are the defaults and change every update
  * @preserve
  */ let Config = {
-    Cookie: {},
+    Cookie: process.env.cookies,
     Ip: '0.0.0.0',
     Port: 8444,
     BufferSize: 8,
@@ -768,17 +768,17 @@ const Proxy = Server((async (req, res) => {
                 ...userConfig
             };
         } else {
-            let envCookies = process.env.cookies;
-            let cookieArr = envCookies.split(/;\s?/gi).filter((prop => false === /^(path|expires|domain|HttpOnly|Secure|SameSite)[=;]*/i.test(prop)));
-            for (const cookie of cookieArr) {
-                const divide = cookie.split(/^(.*?)=\s*(.*)/);
-                const cookieName = divide[1];
-                const cookieVal = divide[2];
-                Config.Cookie[cookieName] = cookieVal;
-            }
+            // let envCookies = process.env.cookies;
+            // let cookieArr = envCookies.split(/;\s?/gi).filter((prop => false === /^(path|expires|domain|HttpOnly|Secure|SameSite)[=;]*/i.test(prop)));
+            // for (const cookie of cookieArr) {
+            //     const divide = cookie.split(/^(.*?)=\s*(.*)/);
+            //     const cookieName = divide[1];
+            //     const cookieVal = divide[2];
+            //     Config.Cookie[cookieName] = cookieVal;
+            // }
             //error
             //Cannot create property 'sessionKey' on string 'sessionKey=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'
-            //Config.Cookie = process.env.cookies;
+            Config.Cookie = process.env.cookies;
             writeSettings(Config, true);
         }
     })();
